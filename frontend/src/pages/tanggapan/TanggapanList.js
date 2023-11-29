183b802609ff27663d6e72a5226ded5b8c6e6a41
GilbyRizki <112262179+GilbyRizki@users.noreply.github.com>
2023-11-29 08:57:01 +0700

    Add files via upload

---
+++ frontend/src/pages/tanggapan/TanggapanList.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar.js";
+import PrimaryButton from "../../components/PrimaryButton.js";
+
+const TanggapanList = () => {
+  const [tanggapans, setTanggapan] = useState([]);
+
+  useEffect(() => {
+    getTanggapan();
+  }, []);
+
+  const getTanggapan = async () => {
+    const response = await axios.get("http://localhost:5000/tanggapan");
+    console.log(response.data)
+    setTanggapan(response.data);
+  };
+
+  const deleteTanggapan = async (id_pengaduan) => {
+    try {
+      await axios.delete(`http://localhost:5000/tanggapan/${id_tanggapan}`);
+      getTanggapan();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container mt-5">
+        <PrimaryButton thisPath={'/addTanggapan'} NamaButton={'Add Tanggapan'} />
+        <div className="columns is-multiline mt-2">
+          {tanggapans.map((tanggapan) => (
+            <div className="column is-one-quarter" key={tanggapan.id_tanggapan}>
+              <div className="card">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-content">
+                      <p className="title is-4">{tanggapan.id_tanggapan}</p>
+                      <p className="title is-4">{tanggapan.nik}</p>
+                      <p className="title is-4">{tanggapan.isi_laporan}</p>
+                      <p className="title is-4">{tanggapan.status}</p>
+                      <p className="title is-4">{tanggapan.tgl_tanggapan}</p>
+                    </div>
+                  </div>
+                </div>
+
+                <footer className="card-footer">
+                  <Link to={`/editTanggapan/${tanggapan.id_tanggapan}`} className="card-footer-item">
+                    Edit
+                  </Link>
+                  <a
+                    onClick={() => deleteTanggapan(tanggapan.id_tanggapan)}
+                    className="card-footer-item"
+                  >
+                    Delete
+                  </a>
+                </footer>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TanggapanList;
